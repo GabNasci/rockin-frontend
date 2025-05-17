5829db35e21e890664ed57222a24ad0893586431
gabnasci <gabriel.nascimento@grupointegrado.br>
2025-05-17 17:24:47 -0300

    feat: redireting to login

--- src/models/profiles/useProfiles.ts
+++ src/models/profiles/useProfiles.ts
@@ -3,12 +3,15 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { createProfile, findProfileByHandle } from "./api";
 import { Profile } from "./types";
 import { toast } from "@/lib/toast";
+import { useRouter } from "next/navigation";
 
 export function useCreateProfile() {
+  const router = useRouter();
   return useMutation({
     mutationFn: (data: Profile) => createProfile(data),
     onSuccess: () => {
       toast.success("Perfil criado com sucesso!");
+      router.push("/login");
     },
   });
 }
